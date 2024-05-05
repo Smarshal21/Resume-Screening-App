6770b12ab34adc2dbd60aa3ebbc6be9c6debbdc8
Smarshal <99678760+Smarshal21@users.noreply.github.com>
2024-05-05 17:51:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-import streamlit as st
-import requests
 from flask import Flask, request, jsonify
 import pickle
 import re
@@ -22,12 +20,11 @@ def clean_resume(resume_text):
     clean_text = re.sub('\s+', ' ', clean_text)
     return clean_text
 
+# Flask app
 app = Flask(__name__)
-@app.route('/hello', methods=['GET'])
-def say_hello():
-    return jsonify({'message': 'Hello, World!'})
-# Streamlit app
-def main():
+
+@app.route('/predict', methods=['POST'])
+def predict():
     if 'file' not in request.files:
         return jsonify({'error': 'No file part'})
 
@@ -81,7 +78,92 @@ def main():
         return jsonify({'predicted_category': category_name})
 
 if __name__ == "__main__":
-    app.run(debug=True, threaded=True, use_reloader=False)
+    app.run(debug=True)
+
+# import streamlit as st
+# import requests
+# from flask import Flask, request, jsonify
+# import pickle
+# import re
+# import nltk
+
+# nltk.download('punkt')
+# nltk.download('stopwords')
+
+# # Loading models
+# clf = pickle.load(open('clf.pkl', 'rb'))
+# tfidfd = pickle.load(open('tfidf.pkl', 'rb'))
+
+# def clean_resume(resume_text):
+#     clean_text = re.sub('http\S+\s*', ' ', resume_text)
+#     clean_text = re.sub('RT|cc', ' ', clean_text)
+#     clean_text = re.sub('#\S+', '', clean_text)
+#     clean_text = re.sub('@\S+', '  ', clean_text)
+#     clean_text = re.sub('[%s]' % re.escape("""!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"""), ' ', clean_text)
+#     clean_text = re.sub(r'[^\x00-\x7f]', r' ', clean_text)
+#     clean_text = re.sub('\s+', ' ', clean_text)
+#     return clean_text
+
+# app = Flask(__name__)
+# @app.route('/hello', methods=['GET'])
+# def say_hello():
+#     return jsonify({'message': 'Hello, World!'})
+# # Streamlit app
+# def main():
+#     if 'file' not in request.files:
+#         return jsonify({'error': 'No file part'})
+
+#     file = request.files['file']
+#     if file.filename == '':
+#         return jsonify({'error': 'No selected file'})
+
+#     if file:
+#         try:
+#             resume_text = file.read().decode('utf-8')
+#         except UnicodeDecodeError:
+#             # If UTF-8 decoding fails, try decoding with 'latin-1'
+#             resume_text = file.read().decode('latin-1')
+
+#         cleaned_resume = clean_resume(resume_text)
+#         input_features = tfidfd.transform([cleaned_resume])
+#         prediction_id = clf.predict(input_features)[0]
+
+#         # Map category ID to category name
+#         category_mapping = {
+#             15: "Java Developer",
+#             23: "Testing",
+#             23: "Testing",
+#             8: "DevOps Engineer",
+#             20: "Python Developer",
+#             24: "Web Designing",
+#             12: "HR",
+#             13: "Hadoop",
+#             3: "Blockchain",
+#             10: "ETL Developer",
+#             18: "Operations Manager",
+#             6: "Data Science",
+#             22: "Sales",
+#             16: "Mechanical Engineer",
+#             1: "Arts",
+#             7: "Database",
+#             11: "Electrical Engineering",
+#             14: "Health and fitness",
+#             19: "PMO",
+#             4: "Business Analyst",
+#             9: "DotNet Developer",
+#             2: "Automation Testing",
+#             17: "Network Security Engineer",
+#             21: "SAP Developer",
+#             5: "Civil Engineer",
+#             0: "Advocate",
+#         }
+
+#         category_name = category_mapping.get(prediction_id, "Unknown")
+
+#         return jsonify({'predicted_category': category_name})
+
+# if __name__ == "__main__":
+#     app.run(debug=True, threaded=True, use_reloader=False)
 
 
 
